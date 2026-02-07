8b8f4b797c7c59f51be50c889fd1a9fbf3561acf
Ignacio <ignaciosalazar986@gmail.com>
2026-02-07 10:51:19 -0300

    queries removidas

--- resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO producto (codigo, articulo, categoria, precio, stock) VALUES ("1234", "Gaseosa 1.5", "Bebidas", 3500, 400);
-INSERT INTO producto (codigo, articulo, categoria, precio, stock) VALUES ("1235", "Soda zifon", "Bebidas", 1500, 700);
-INSERT INTO producto (codigo, articulo, categoria, precio, stock) VALUES ("1236", "Atun", "Enlatados", 2500, 500);
--- resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS producto (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    codigo VARCHAR(30),
-    articulo VARCHAR(60),
-    categoria VARCHAR(40),
-    precio DOUBLE,
-    stock INT
-);
-
